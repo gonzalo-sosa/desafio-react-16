7f2e7d81b48e1f311fc013dead54d568d3bb41c4
gonzalo <gonzalo.sosa@moxit.com.ar>
2024-12-12 16:01:28 -0300

    Esperar un tiempo determinado para ejecutar una función

---
+++ src/utils/debounce.js
@@ -0,0 +1,9 @@
+export default function (fn, delay = 300) {
+  let timer;
+  return function (...args) {
+    clearTimeout(timer);
+    timer = setTimeout(() => {
+      fn.apply(this, args);
+    }, delay);
+  };
+}
