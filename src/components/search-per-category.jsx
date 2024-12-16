8c576e16946afa88c222d853bc4d15584256526b
gonzalo <gonzalo.sosa@moxit.com.ar>
2024-12-16 10:57:38 -0300

    Clase modificada

--- src/components/search-per-category.jsx
+++ src/components/search-per-category.jsx
@@ -2,7 +2,7 @@ const SearchPerCategory = ({ items, onSelectOption }) => {
   const options = [{ value: '', label: ' ' }, ...items];
 
   return (
-    <form className="search--filter">
+    <form className="search__filter">
       <select onChange={(e) => onSelectOption(e.target.value)}>
         {options.map((opt, index) => (
           <option key={index} value={opt.value} >
