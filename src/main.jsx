d3c7106e55a1130ff09ede5b666879b64b492eaa
gonzalo <gonzalo.sosa@moxit.com.ar>
2024-12-17 11:15:34 -0300

    Uso de bootstrap

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,7 @@ import { createRoot } from 'react-dom/client';
 import './index.css';
 import App from './App.jsx';
 import 'react-toastify/ReactToastify.min.css';
+import 'bootstrap/dist/css/bootstrap.css';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
