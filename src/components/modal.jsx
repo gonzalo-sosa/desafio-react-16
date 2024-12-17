4d31d128ac1e08815b73623e0f823c86344755ad
gonzalo <gonzalo.sosa@moxit.com.ar>
2024-12-17 11:14:59 -0300

    Ajustados estilos y contenido de modal

--- src/components/modal.jsx
+++ src/components/modal.jsx
@@ -5,31 +5,38 @@ class Modal extends Component {
   render() {
     const { title, onClose } = this.props;
 
-    const [FigureContent, AsideContent] = this.props.children;
+    const [PrimaryContent, SecondaryContent] = this.props.children;
 
     return (
-      <motion.div initial={{ opacity: 0, scale: 0.8}} animate={{ opacity: 1, scale: 1 }}
-         className="modal" tabIndex="-1">
-        <div className="modal-content">
-          <header className="modal-header">
-            <h5 className="modal-title">{title}</h5>
-            <button
-              type="button"
-              className="btn-close"
-              data-bs-dismiss="modal"
-              aria-label="Close"
-              onClick={() => onClose()}
-            >
-              X
-            </button>
-          </header>
-          <div className="modal-body">
-            <aside className='modal-aside'>
-              {AsideContent}
-            </aside>
-            <figure>
-              {FigureContent}
-            </figure>
+      <motion.div
+        initial={{ opacity: 0, scale: 0.8 }}
+        animate={{ opacity: 1, scale: 1 }}
+        className="modal"
+        style={{ display: 'block' }}
+        tabIndex="-1"
+      >
+        <div className="modal-dialog modal-dialog-centered">
+          <div className="modal-content">
+            <div className="modal-header">
+              <h5 className="modal-title">{title}</h5>
+              <button
+                type="button"
+                className="btn-close"
+                data-bs-dismiss="modal"
+                aria-label="Close"
+                onClick={() => onClose()}
+              ></button>
+            </div>
+            <div className="modal-body">
+              <div className="container-fluid">
+                <div className="row">
+                  <div className="col">{PrimaryContent}</div>
+                </div>
+                <div className="row">
+                  <div className="col">{SecondaryContent}</div>
+                </div>
+              </div>
+            </div>
           </div>
         </div>
       </motion.div>
